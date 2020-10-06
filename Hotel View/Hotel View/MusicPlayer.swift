ebfa8c67885b521db31298d39375a99c8ecad469
Matthew Harris <mrharris@jpl.nasa.gov>
2020-10-06 16:50:16 -0700

    New class to help loop music properly, also adding loop from Disney hotels just because its delightful

---
+++ Hotel View/Hotel View/MusicPlayer.swift
@@ -0,0 +1,29 @@
+//
+//  MusicPlayer.swift
+//  Hotel View
+//
+//  Created by Matthew Harris on 10/6/20.
+//
+
+import Foundation
+import AVFoundation
+
+class MusicPlayer {
+    static let shared = MusicPlayer()
+    var audioPlayer: AVAudioPlayer?
+
+    func startBackgroundMusic() {
+        if let bundle = Bundle.main.path(forResource: "backgroundMusic", ofType: "mp3") {
+            let backgroundMusic = NSURL(fileURLWithPath: bundle)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf:backgroundMusic as URL)
+                guard let audioPlayer = audioPlayer else { return }
+                audioPlayer.numberOfLoops = -1
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
